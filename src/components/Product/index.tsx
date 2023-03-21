a740dc031ce859da7c01ea80f3839afa34be3e96
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2023-03-21 11:51:56 -0300

    Feature: Adicionado as estrelas de avaliações no Card do produto e o valor do disconto

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -2,10 +2,13 @@ import { IProduct } from "@/interfaces/IProduct";
 import {
   ProductContainer,
   ProductImage,
+  ProductPercentDiscount,
   ProductPrices,
+  ProductRating,
   ProductTitle,
 } from "@/styles/components/product";
 import { formatCurrencyBRL } from "@/utils/currencyFormat";
+import StarRatings from "react-star-ratings";
 import Image from "next/image";
 
 interface ProductProps {
@@ -13,9 +16,16 @@ interface ProductProps {
 }
 
 export function Product({ product }: ProductProps) {
+  const discount = 100 - Math.floor((product.price * 100) / product.oldPrice);
+
   return (
     <ProductContainer>
       <ProductImage>
+        {discount > 0 && (
+          <ProductPercentDiscount>
+            {String(discount).padStart(2, "0")}%
+          </ProductPercentDiscount>
+        )}
         <Image src={product.image} alt="" width={120} height={150} />
       </ProductImage>
       <ProductTitle>{product.title}</ProductTitle>
@@ -23,6 +33,15 @@ export function Product({ product }: ProductProps) {
         <span>{formatCurrencyBRL.format(product.price)}</span>
         <span>{formatCurrencyBRL.format(product.oldPrice)}</span>
       </ProductPrices>
+      <ProductRating>
+        <StarRatings
+          rating={product.rating.rate}
+          starRatedColor="#FF6650"
+          starDimension="16px"
+          starSpacing="1px"
+        />
+        <span>({product.rating.count})</span>
+      </ProductRating>
     </ProductContainer>
   );
 }
--- src/styles/components/product.ts
+++ src/styles/components/product.ts
@@ -4,10 +4,15 @@ export const ProductContainer = styled("div", {
   display: "flex",
   flexDirection: "column",
   alignItems: "center",
-  //   justifyContent: "center",
   padding: "16px",
   border: "1px solid $zinc-300",
   borderRadius: "16px",
+  cursor: "pointer",
+  boxShadow: "0px 0px 10px 0px rgba(0, 0, 0, 0.1)",
+
+  "&:hover": {
+    background: "$zinc-100",
+  },
 });
 
 export const ProductImage = styled("div", {
@@ -19,12 +24,24 @@ export const ProductImage = styled("div", {
   background: "$zinc-200",
   borderRadius: "16px",
   padding: "16px",
+  position: "relative",
+});
+
+export const ProductPercentDiscount = styled("span", {
+  position: "absolute",
+  top: "16px",
+  right: "16px",
+  background: "$primary",
+  color: "$white",
+  padding: "4px 8px",
+  borderRadius: "8px",
+  fontSize: "12px",
 });
 
 export const ProductTitle = styled("h3", {
   fontSize: "16px",
   textAlign: "center",
-  margin: "8px 0",
+  marginTop: "8px",
   whiteSpace: "nowrap",
   overflow: "hidden",
   textOverflow: "ellipsis",
@@ -36,6 +53,7 @@ export const ProductPrices = styled("div", {
   flexDirection: "row",
   alignItems: "center",
   gap: "8px",
+  marginTop: "4px",
 
   "span:first-child": {
     fontSize: "16px",
@@ -47,5 +65,18 @@ export const ProductPrices = styled("div", {
     fontSize: "14px",
     color: "$zinc-400",
     textDecoration: "line-through",
+  },
+});
+
+export const ProductRating = styled("div", {
+  display: "flex",
+  flexDirection: "row",
+  alignItems: "center",
+  gap: "4px",
+  marginTop: "4px",
+
+  span: {
+    fontSize: "12px",
+    color: "$zinc-400",
   },
 });
